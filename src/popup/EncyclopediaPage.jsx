6aa4ff616fddcf9a3d02a86aec9f00c488982fb3
Ben Horgan <benhorgan68@gmail.com>
2022-01-07 20:59:24 -0500

    styling adjustments

--- src/popup/EncyclopediaPage.jsx
+++ src/popup/EncyclopediaPage.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect, useRef, useState } from "react";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import React, { useEffect, useRef, useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import {
   EncyclopediaContainer,
   AllBirdsContainer,
-  CenteredFlexColumn,
-} from "./styles";
-import { birdTypes } from "../contentScripts/birdType";
-import { localURL } from "../contentScripts/utils";
+  BirdInfoContainer,
+} from './styles';
+import { birdTypes } from '../contentScripts/birdType';
+import { localURL } from '../contentScripts/utils';
 
 const EncyclopediaPage = () => {
   const [selectedBirdType, setSelectedBirdType] = useState();
+  const [showBird, setShowBird] = useState(false);
   const [pageIndex, setPageIndex] = useState(0);
   const [birdsSeen, setBirdsSeen] = useState({});
 
   useEffect(() => {
     chrome.storage.local.get({ birdsSeen: {} }, ({ birdsSeen }) => {
       setBirdsSeen(birdsSeen);
-      chrome.runtime.onMessage.addListener((msg) => {
+      chrome.runtime.onMessage.addListener(msg => {
         if (msg.seen) {
-          setBirdsSeen((previousBirdsSeen) => {
-            return { [msg.seen]: true, ...previousBirdsSeen };
+          setBirdsSeen(previousBirdsSeen => {
+            return { [msg.seen]: new Date(), ...previousBirdsSeen };
           });
         }
       });
@@ -34,8 +35,8 @@ const EncyclopediaPage = () => {
   birdsByRarity.sort(compareBirds);
 
   for (const birdType of birdsByRarity) {
-    const gridBirdStyle = { width: "35px", imageRendering: "pixelated" };
-    const grayedOutFilter = "contrast(0) brightness(150%)";
+    const gridBirdStyle = { width: '35px', imageRendering: 'pixelated' };
+    const grayedOutFilter = 'contrast(0) brightness(150%)';
     const grayedOutStyle = {
       WebkitFilter: grayedOutFilter,
       filter: grayedOutFilter,
@@ -44,51 +45,58 @@ const EncyclopediaPage = () => {
       Object.assign(gridBirdStyle, grayedOutStyle);
     }
     const birdImage = (
-      <div>
+      <div
+        style={{ padding: '6px', display: 'flex', justifyContent: 'center' }}
+        onMouseEnter={() => {
+          setSelectedBirdType(birdType);
+          setShowBird(true);
+        }}
+        onMouseLeave={() => {
+          setShowBird(false);
+        }}
+      >
         <img
           src={localURL(`images/birds/${birdType.imagePath}/standing.png`)}
           style={gridBirdStyle}
-          onMouseEnter={() => {
-            setSelectedBirdType(birdType);
-          }}
-          onMouseLeave={() => {
-            setSelectedBirdType();
-          }}
         />
       </div>
     );
     allBirds.push(birdImage);
   }
 
+  const birdSeen =
+    selectedBirdType && birdsSeen[selectedBirdType.imagePath] ? true : false;
+
   return (
     <EncyclopediaContainer>
       <AllBirdsContainer>{allBirds}</AllBirdsContainer>
-
-      {selectedBirdType && birdsSeen[selectedBirdType.imagePath] ? (
-        <CenteredFlexColumn>
-          <h2 style={{ margin: 0, textAlign: "center" }}>
-            {selectedBirdType.name}
-          </h2>
-          <h4
-            style={{
-              margin: 0,
-              textAlign: "center",
-              fontStyle: "italic",
-            }}
-          >
-            {selectedBirdType.species}
-          </h4>
-          <img
-            src={localURL(
-              `images/birds/${selectedBirdType.imagePath}/standing.png`
-            )}
-            style={{
-              width: "40px",
-              imageRendering: "pixelated",
-            }}
-          />
-        </CenteredFlexColumn>
-      ) : null}
+      <BirdInfoContainer toShow={showBird && birdSeen}>
+        {birdSeen && (
+          <>
+            <h2 style={{ margin: 0, textAlign: 'center' }}>
+              {selectedBirdType.name}
+            </h2>
+            <h4
+              style={{
+                margin: 0,
+                textAlign: 'center',
+                fontStyle: 'italic',
+              }}
+            >
+              {selectedBirdType.species}
+            </h4>
+            <img
+              src={localURL(
+                `images/birds/${selectedBirdType.imagePath}/standing.png`
+              )}
+              style={{
+                width: '40px',
+                imageRendering: 'pixelated',
+              }}
+            />
+          </>
+        )}
+      </BirdInfoContainer>
     </EncyclopediaContainer>
   );
 };
--- src/popup/styles.js
+++ src/popup/styles.js
@@ -1,7 +1,7 @@
-import { localURL } from "../contentScripts/utils";
-import { createTheme, styled } from "@mui/material/styles";
-import { cyan, pink } from "@mui/material/colors";
-import { ToggleButton } from "@mui/material";
+import { localURL } from '../contentScripts/utils';
+import { createTheme, styled } from '@mui/material/styles';
+import { cyan, pink } from '@mui/material/colors';
+import { ToggleButton } from '@mui/material';
 
 export const theme = createTheme({
   palette: {
@@ -9,7 +9,7 @@ export const theme = createTheme({
     secondary: pink,
   },
   typography: {
-    fontFamily: ["Quicksand"],
+    fontFamily: ['Quicksand'],
     body1: {
       fontWeight: 500,
     },
@@ -20,7 +20,7 @@ export const theme = createTheme({
         @font-face {
           font-family: "Domine";
           src: url(${localURL(
-            "fonts/Domine/Domine-VariableFont_wght.ttf"
+            'fonts/Domine/Domine-VariableFont_wght.ttf'
           )}) format("truetype");
         }
         @font-face {
@@ -34,52 +34,58 @@ export const theme = createTheme({
   },
 });
 
-export const PopupContainer = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  backgroundColor: "#f0fdff",
-  width: "300px",
+export const PopupContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  backgroundColor: '#f0fdff',
+  width: '300px',
 }));
 
-export const BirdInspectorContainer = styled("div")(({ theme }) => ({
-  display: "flex",
-  justifyContent: "space-between",
-  fontFamily: "Domine",
+export const BirdInspectorContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  fontFamily: 'Domine',
 }));
 
-export const EncyclopediaContainer = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  fontFamily: "Domine",
+export const EncyclopediaContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  fontFamily: 'Domine',
 }));
 
-export const AllBirdsContainer = styled("div")(({ theme }) => ({
-  display: "grid",
-  gridTemplateColumns: "repeat(5, 1fr)",
-  alignItems: "center",
-  padding: "15px",
-  gap: "12px",
+export const AllBirdsContainer = styled('div')(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(5, 1fr)',
+  alignItems: 'center',
+  padding: '9px',
+  // gap: '12px',
 }));
 
-export const CenteredFlexColumn = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
+export const CenteredFlexColumn = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 }));
 
-export const CheckboxContainer = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "flex-start",
-  whiteSpace: "nowrap",
+export const BirdInfoContainer = styled(CenteredFlexColumn)(({ toShow }) => ({
+  maxHeight: toShow ? '10rem' : '0',
+  transition: 'max-height 0.3s',
+  overflow: 'hidden',
 }));
 
-export const TabPanelContainer = styled("div")(({ theme }) => ({
-  padding: "0.5rem",
+export const CheckboxContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  whiteSpace: 'nowrap',
+}));
+
+export const TabPanelContainer = styled('div')(({ theme }) => ({
+  padding: '0.5rem',
 }));
 
 export const WideToggleButton = styled(ToggleButton)(() => ({
-  padding: "0 11px",
+  padding: '0 11px',
 }));
